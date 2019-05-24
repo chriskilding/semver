96e22f3536c5de7a294f82caabd1328e583eafb6
Chris Kilding <user@fake>
2019-05-25 00:59:33 +0100

    Ignore single shellcheck false positive

--- semver.sh
+++ semver.sh
@@ -9,18 +9,21 @@ set -o pipefail
 
 ##
 # Increment the major version component of a Semantic Version string.
+# shellcheck disable=SC2039
 ++major() {
     semver printf '%major %minor %patch' "$1" | awk '{ print ++$1 "." 0 "." 0 }'
 }
 
 ##
 # Increment the minor version component of a Semantic Version string.
+# shellcheck disable=SC2039
 ++minor() {
     semver printf '%major %minor %patch' "$1" | awk '{ print $1 "." ++$2 "." 0 }'
 }
 
 ##
 # Increment the patch version component of a Semantic Version string.
+# shellcheck disable=SC2039
 ++patch() {
     semver printf '%major %minor %patch' "$1" | awk '{ print $1 "." $2 "." ++$3 }'
 }
